6f398ed3376bbf7a82e1841aa3150f052df29b9a
Abdul Qadir Soomro <abdulqadir53970@gmail.com>
2025-10-29 00:39:04 +0500

    Added Hostinger API Proxy

--- api/hostinger.js
+++ api/hostinger.js
@@ -1,7 +1,6 @@
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
-  // Allow CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
