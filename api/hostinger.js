1e927ece4c2c0904a306560dd482508635b637e2
Abdul Qadir Soomro <abdulqadir53970@gmail.com>
2025-10-28 22:54:28 +0500

    Added Hostinger API route

--- api/hostinger.js
+++ api/hostinger.js
@@ -1,6 +1,5 @@
 export default async function handler(req, res) {
   if (req.method === "OPTIONS") {
-    // Preflight CORS Support
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
